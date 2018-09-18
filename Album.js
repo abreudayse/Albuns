da5e49aa10fcd9956662dc288782f7eeecae540a
Dayse Abreu <abreudayse@gmail.com>
2018-09-18 16:10:31 -0300

    Add photo to Albums details.

--- Album.js
+++ Album.js
@@ -3,16 +3,17 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Image
 } from 'react-native';
 
 export default class Album extends Component {
     render() {
         const details = this.props.album.style + ' / ' + this.props.album.year;
-        console.log(this.props.album);
         return (
             <View style={styles.containerAlbum}>
                 <View style={{marginHorizontal: 10, marginBottom: 10, borderBottomColor: '#737373', borderBottomWidth: 0.5, paddingBottom: 4, paddingTop: 8}}>
+                    <Image style={{width: 64, height: 64}} source={{uri: this.props.album.avatar}}/>
                     <Text style={{fontSize: 16, color: '#262626'}}>{this.props.album.name}</Text>
                     <Text style={{fontSize: 14, color: '#737373'}}>{details}</Text>
                 </View>
--- App.js
+++ App.js
@@ -24,22 +24,26 @@ export default class App extends Component {
       {
         name: 'Britney Spears',
         year: '2002',
-        style: 'POP'
+        style: 'POP',
+        avatar: 'https://images.recordsale.de/256/256/britneyspears-inthezone(1).jpg'
       },
       {
         name: 'Michael Jackson',
         year: '1999',
-        style: 'POP'
+        style: 'POP',
+        avatar: 'https://pm1.narvii.com/6592/0559113767f2a3078f90096da338c55b239fe747_128.jpg'
       },
       {
         name: 'Link Park',
         year: '2004',
-        style: 'Rock'
+        style: 'Rock',
+        avatar: 'https://pm1.narvii.com/6480/43326d4c7bd4ad6bc63162f1a4294877d6817936_128.jpg'
       },
       {
         name: 'Roberto Carlos',
         year: '2008',
-        style: 'MPB'
+        style: 'MPB',
+        avatar: 'https://c-sf.smule.com/sf/s51/arr/00/90/1cf477c6-38d0-4e8b-82dd-a095d6ab754b.jpg'
       },
     ]
 
@@ -53,7 +57,7 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Header title='Albuns list'/>
+        <Header title='Albums List'/>
         <ListView
           dataSource={this.state.dataSource}
           renderRow={this.renderAlbum}
@@ -85,7 +89,7 @@ const styles = StyleSheet.create({
     fontStyle: 'italic'
   },
   textHeader: {
-    fontSize: 16,
+    fontSize: 18,
     color: 'black'
   }
 
