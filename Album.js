68c5c9c61d855e9b9382df8a0b8b635d7eae102b
Dayse Abreu <abreudayse@gmail.com>
2018-09-18 23:30:45 -0300

    Update album styles.

--- Album.js
+++ Album.js
@@ -11,11 +11,11 @@ export default class Album extends Component {
     render() {
         const details = this.props.album.style + ' / ' + this.props.album.year;
         return (
-            <View style={styles.containerAlbum}>
-                <View style={{marginHorizontal: 10, marginBottom: 10, borderBottomColor: '#737373', borderBottomWidth: 0.5, paddingBottom: 4, paddingTop: 8}}>
-                    <Image style={{width: 64, height: 64}} source={{uri: this.props.album.avatar}}/>
-                    <Text style={{fontSize: 16, color: '#262626'}}>{this.props.album.name}</Text>
-                    <Text style={{fontSize: 14, color: '#737373'}}>{details}</Text>
+            <View style={styles.container}>
+                <View style={styles.containerAlbum}>
+                    <Image style={styles.avatar} source={{uri: this.props.album.avatar}}/>
+                    <Text style={styles.textName}>{this.props.album.name}</Text>
+                    <Text style={styles.textDetails}>{details}</Text>
                 </View>
             </View>
         )
@@ -23,9 +23,28 @@ export default class Album extends Component {
 }
 
 const styles = StyleSheet.create({
-    containerAlbum: {
+    container: {
         backgroundColor: '#f2f2f2',
-
+    },
+    containerAlbum: {
+        marginHorizontal: 10, 
+        marginBottom: 10, 
+        borderBottomColor: '#737373', 
+        borderBottomWidth: 0.5, 
+        paddingBottom: 4, 
+        paddingTop: 8
+    },
+    avatar: {
+        width: 64, 
+        height: 64
+    },
+    textName: {
+        fontSize: 16, 
+        color: '#262626'
+    },
+    textDetails: {
+        fontSize: 14, 
+        color: '#737373'
     }
 });
   
